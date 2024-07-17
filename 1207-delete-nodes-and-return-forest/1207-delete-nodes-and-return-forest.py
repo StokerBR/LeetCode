a41cd51a589bb5d608f111aa6e054b98cb2d829f
Henrique Costa <51726192+StokerBR@users.noreply.github.com>
2024-07-17 14:29:52 -0300

    Time: 63 ms (17.78%), Space: 16.9 MB (61.78%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -6,34 +6,31 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        delete = set(to_delete)
-        res = []
+        trees = []
+
         def dfs(node):
-            if not node:
+            if not node.right and not node.left:
                 return
-
-            dfs(node.left)
-            dfs(node.right)
-
-            if node.left and node.left.val in delete:
-                if node.left.left:
-                    res.append(node.left.left)
-                if node.left.right:
-                    res.append(node.left.right)
-                node.left = None
-            if node.right and node.right.val in delete:
-                if node.right.left:
-                    res.append(node.right.left)
-                if node.right.right:
-                    res.append(node.right.right)
-                node.right = None
-
-        dfs(root)
-        if root.val not in delete:
-            res.append(root)
-        else:
-            if root.right:
-                res.append(root.right)
-            if root.left:
-                res.append(root.left)
-        return res
+            
+            if node.left:
+                dfs(node.left)
+                if node.left.val in to_delete:
+                    if node.left.left and node.left.left.val not in to_delete:
+                        trees.append(node.left.left)
+                    if node.left.right and node.left.right.val not in to_delete:
+                        trees.append(node.left.right)
+                    node.left = None
+            
+            if node.right:
+                dfs(node.right)
+                if node.right.val in to_delete:
+                    if node.right.left and node.right.left.val not in to_delete:
+                        trees.append(node.right.left)
+                    if node.right.right and node.right.right.val not in to_delete:
+                        trees.append(node.right.right)
+                    node.right = None
+        
+        dfs(TreeNode(None, root))
+        if root.val not in to_delete:
+            trees.insert(0, root)
+        return trees
